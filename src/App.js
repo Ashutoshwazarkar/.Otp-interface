3c3b6ea1d21f6ce95a3deda95cf7ef2260f4660b
Ashutoshwazarkar <ashutoshwazarkar1@gmail.com>
2024-01-26 15:38:09 +0530

    c1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import Board from './components/Board';
 function App() {
+  const {board , setBoard} = useState(Array(9).fill(null));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board board ={board} onClick={null}/>
     </div>
   );
 }
